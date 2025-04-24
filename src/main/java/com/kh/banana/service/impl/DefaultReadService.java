01849e76502cb5535441fbb56bc2e5b8c3e12672
cjh <soyre1928@gmail.com>
2025-04-25 00:23:00 +0900

    게시글 삭제 기능 오류

--- src/main/java/com/kh/banana/service/impl/DefaultReadService.java
+++ src/main/java/com/kh/banana/service/impl/DefaultReadService.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.kh.banana.entity.PostEntity;
+import com.kh.banana.repository.CommentRepository;
 import com.kh.banana.repository.PostRepository;
 import com.kh.banana.service.ReadService;
 
@@ -14,9 +15,13 @@ import lombok.RequiredArgsConstructor;
 public class DefaultReadService implements ReadService{
 	
 	private final PostRepository postRepo;
+	private final CommentRepository commentRepo;
 	
 	@Override
 	public ResponseEntity<?> deletePost(Long postId) {
+		
+		commentRepo.deleteById(postId);
+		
 		PostEntity post = postRepo.findById(postId).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다."));
 		postRepo.delete(post);
 		return ResponseEntity.ok("성공!");
