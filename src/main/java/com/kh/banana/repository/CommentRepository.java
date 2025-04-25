a84ddffee0a036ff1e74567b3a993f743586d75b
kokeoung <ko908599@gmail.com>
2025-04-25 19:23:11 +0900

    댓글작성 구현

--- src/main/java/com/kh/banana/repository/CommentRepository.java
+++ src/main/java/com/kh/banana/repository/CommentRepository.java
@@ -4,7 +4,10 @@ import com.kh.banana.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
     List<CommentEntity> findByPostId(Long postId);
+
+    Optional<CommentEntity> findById(Long commentId);
 }
--- src/main/java/com/kh/banana/service/CommentService.java
+++ src/main/java/com/kh/banana/service/CommentService.java
@@ -5,9 +5,12 @@ import com.kh.banana.dto.response.CommentResponseDTO;
 import com.kh.banana.entity.CommentEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentService {
     CommentEntity saveComment(CommentRequestDTO requestDTO, Long postId, Long userId);
 
     List<CommentResponseDTO> getCommentsByPostId(Long postId);
+
+    void deleteComments(Long commentId);
 }
